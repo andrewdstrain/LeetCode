4f97d980e0e77670581bb543c703bf6069cff259
Andrew Strain <andrew.d.strain@outlook.com>
2020-10-19 10:17:23 -0400

    Add another test

--- twitter/src/TwitterTest.java
+++ twitter/src/TwitterTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 @DisplayName("Twitter")
 public class TwitterTest {
     @Test
-    void test() {
+    void leetCodeTest() {
         Twitter twitter = new Twitter();
         List<Integer> answer = new ArrayList<>();
 
@@ -23,6 +23,37 @@ public class TwitterTest {
 
         twitter.unfollow(1, 2);
         answer.remove(0);
+        Assertions.assertEquals(answer, twitter.getNewsFeed(1));
+    }
+
+    @Test
+    void onlyTen() {
+        Twitter twitter = new Twitter();
+        List<Integer> answer = new ArrayList<>();
+
+        twitter.postTweet(1, 1);
+        twitter.postTweet(1, 2);
+        twitter.postTweet(1, 3);
+        twitter.postTweet(1, 4);
+        twitter.postTweet(1, 5);
+        twitter.postTweet(1, 6);
+        twitter.postTweet(1, 7);
+        twitter.postTweet(1, 8);
+        twitter.postTweet(1, 9);
+        twitter.postTweet(1, 10);
+        twitter.postTweet(1, 11);
+
+        answer.add(11);
+        answer.add(10);
+        answer.add(9);
+        answer.add(8);
+        answer.add(7);
+        answer.add(6);
+        answer.add(5);
+        answer.add(4);
+        answer.add(3);
+        answer.add(2);
+
         Assertions.assertEquals(answer, twitter.getNewsFeed(1));
     }
 }
